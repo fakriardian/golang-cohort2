634031473c5a6a6a078eb9796ce31080f2f17e00
fakriardian <mazbro9988@gmail.com>
2023-10-18 21:58:16 +0700

    update model

--- models/item.go
+++ models/item.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Item struct {
-	ID          uuid.UUID `gorm:"column:id; primaryKey" json:"id,omitempty"`
-	Name        string    `gorm:"column:name; not null" json:"name"`
-	Description string    `gorm:"column:description; not null" json:"description"`
-	Quantity    int       `gorm:"column:quantity; not null" json:"quantity"`
-	OrderID     uuid.UUID `gorm:"type:varchar(191); primaryKey" json:"orderId"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	ID          uuid.UUID  `gorm:"column:id; primaryKey" json:"id,omitempty"`
+	Name        string     `gorm:"column:name; not null" json:"name"`
+	Description string     `gorm:"column:description; not null" json:"description"`
+	Quantity    int        `gorm:"column:quantity; not null" json:"quantity"`
+	OrderID     uuid.UUID  `gorm:"type:varchar(191); primaryKey" json:"orderId"`
+	CreatedAt   *time.Time `json:"createdAt"`
+	UpdatedAt   *time.Time `json:"updatedAt"`
 }
 
 func (Item) TableName() string {
--- models/order.go
+++ models/order.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Order struct {
-	ID           uuid.UUID `gorm:"column:id; primaryKey" json:"id,omitempty"`
-	CustomerName string    `gorm:"column:customer_name; not null" json:"customerName"`
-	OrederedAt   time.Time `gorm:"column:ordered_at; not null" json:"orderedAt"`
-	Items        []Item    `json:"items"`
-	CreatedAt    time.Time `json:"createdAt"`
-	UpdatedAt    time.Time `json:"updatedAt"`
+	ID           uuid.UUID  `gorm:"column:id; primaryKey" json:"id,omitempty"`
+	CustomerName string     `gorm:"column:customer_name; not null" json:"customerName"`
+	OrederedAt   time.Time  `gorm:"column:ordered_at; not null" json:"orderedAt"`
+	Items        []Item     `json:"items"`
+	CreatedAt    *time.Time `json:"createdAt"`
+	UpdatedAt    *time.Time `json:"updatedAt"`
 }
 
 func (Order) TableName() string {
